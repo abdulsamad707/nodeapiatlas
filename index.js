7588c7383d69be0d860dffe6f0dd0249dbcbc4af
abdulsamad707 <abdulsamadahsan@gmal.com>
2022-10-19 22:17:08 -0700

    users api

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express =require("express");
 const app =express();
 const mongoose=require("mongoose");
-const uri = "mongodb+srv://Unigrocery:jHhUHk29D78oS6iI@cluster0.2zyy2qt.mongodb.net/groceryshop?retryWrites=true&w=majority";
+const products=require('./products');
+const users=require('./users');
+const cors=require("cors");
+  app.use(cors());
+/*srv*
+mongodb://Unigrocery:xgZHEW2MlTp3oRYR@cluster0.2zyy2qt.mongodb.net/groceryshop?retryWrites=true&w=majority*/
+
+const uri = "mongodb://127.0.0.1/grocerystore";
 
    mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
-        curretTime=new Date();
-             
-        console.log(`connection succesfully at ${curretTime} Pakistan Aisa`);
+  
+            app.get('/products',function(req,res){
+                products.find({},function(err,products){
+                      console.log(products);
+                       res.json(products);
+                  });
+            });
+            app.get('/users',function(req,res){
+                users.find({},function(err,users){
+                       res.json(users);
+                  });
+            });
+            app.get('*',(req,res)=>{
+            
+                   res.json([{"message":"Error"}]);
+            });
 
+    }).catch((err)=>{
+
+        console.log("connection failed");
     });
- app.listen(5000);
-    
+ 
+    app.listen(5000);
 
 
--- users.js
+++ users.js
@@ -1,7 +1,8 @@
 const mongoose=require("mongoose");
 let productsSchema=new mongoose.Schema({
     _id:mongoose.Schema.Types.ObjectId,
-    name:String,
-    price:Number
+    username:String,
+    password:String,
+    activestatus:Number
 });
-module.exports=mongoose.model('products',productsSchema);
+module.exports=mongoose.model('users',productsSchema);
