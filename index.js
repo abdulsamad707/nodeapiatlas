9385780eebbc469d7ebd325f558156b5c151e9fa
abdulsamad707 <abdulsamadahsan@gmal.com>
2022-10-20 08:15:37 -0700

    register api

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const mongoose=require("mongoose");
 const products=require('./products');
 const users=require('./users');
 const cors=require("cors");
+
   app.use(cors());
+  app.use(express.json());
 /*srv*
 mongodb://Unigrocery:xgZHEW2MlTp3oRYR@cluster0.2zyy2qt.mongodb.net/groceryshop?retryWrites=true&w=majority*/
 
@@ -12,16 +14,30 @@ const uri = "mongodb://127.0.0.1/grocerystore";
 
    mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
   
-            app.get('/products',function(req,res){
+            app.get('/products',async function(req,res){
+                  let ProductData=await products.find();
+                   res.send(ProductData);
+                
+                   /*
                 products.find({},function(err,products){
                       console.log(products);
                        res.json(products);
-                  });
+                  });*/
             });
-            app.get('/users',function(req,res){
-                users.find({},function(err,users){
+            app.get('/users',async (req,res)=>{
+              /*  users.find({},function(err,users){
                        res.json(users);
                   });
+                  */
+                 userData=await users.find();
+                 res.send(userData);
+            });
+            app.post("/registeruser", async (reqe,res)=>{
+              let userRegisterStatus=new users(reqe.body);
+         
+               let result =await userRegisterStatus.save();
+                res.send({"message":"Data Save"});
+
             });
             app.get('*',(req,res)=>{
             
--- users.js
+++ users.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 let productsSchema=new mongoose.Schema({
-    _id:mongoose.Schema.Types.ObjectId,
+  
     username:String,
     password:String,
     activestatus:Number
