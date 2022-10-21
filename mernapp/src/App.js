07d9c9d4fb2efd24a581ab1ad7e5791823b60e4b
abdulsamad707 <abdulsamadahsan@gmal.com>
2022-10-20 20:44:40 -0700

    Mern app

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React,{useEffect,useState} from "react";
+import User from './header';
+
+
+
+  function App() {
+
+    const [data, setdata] = useState([]);
+   
+
+    useEffect(() => {
+
+      
+      fetch("http://127.0.0.1:5000/users").then((result)=>{
+        result.json().then((resp)=>{
+  
+         setdata(resp);
+            
+        });
+      });
+  
+    
+    },[])
+
+
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+       <>  
+   
+         
+        
+           {data.map((item,index)=>
+                     <User data={item} key={index}/>
+                
+           )}
+        
+       </>
   );
 }
 
--- users.js
+++ users.js
@@ -2,6 +2,7 @@ const mongoose=require("mongoose");
 let productsSchema=new mongoose.Schema({
   
     username:String,
+    email:String,
     password:String,
     activestatus:Number
 });
